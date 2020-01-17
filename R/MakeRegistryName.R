d167bb8560f224284f65f2127f5abcfe27cada77
Are Edvardsen <biorakel@gmail.com>
2020-01-17 10:54:31 +0100

    re-write and according to noric

--- R/MakeRegistryName.R
+++ R/MakeRegistryName.R
@@ -6,27 +6,16 @@
 #' @param baseName String giving the prefix base of the name
 #' @param reshID String providing the current reshID. At Rapporteket, reshID
 #' should already be present in the current R session
-#' @param role String defining the current role of the user. Defaults to "LU"
-#' @param localRegistry Logical that if TRUE (default) make the function return
-#' a registry name corresponing to the local registry. If FALSE the function
-#' will try to return the name of the national registry falling back to the
-#' local registry name if not successfully so
-#' @return registryName String containing the registry name as used in config
+#' @return String containing a valid registry name
 #' @export
 
-makeRegistryName <- function(baseName, reshID = reshID, role = "LU",
-                                  localRegistry = TRUE) {
+makeRegistryName <- function(baseName, reshID=reshID) {
 
-  if (localRegistry) {
-    return(paste0(baseName, reshID))
-  } else {
+  if (isNationalReg(reshID)) {
     conf <- rapbase::getConfig(fileName = "rapbaseConfig.yml")
-    if (reshID == conf$reg$smerte$nationalAccess$reshId &&
-        role == conf$reg$smerte$nationalAccess$userRole) {
-      return(paste0(baseName, conf$reg$smerte$nationalAccess$nameKey))
-    } else {
-      warning("Someting is fishy! Falling back to local registry name")
-      return(paste0(baseName, reshID))
-    }
+    return(paste0(baseName, conf$reg$noric$nationalAccess$nameKey))
+  } else {
+    return(paste0(baseName, reshID))
   }
+
 }
