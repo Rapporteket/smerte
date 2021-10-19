c2b09ed0034334b9ef0ef35057497acee80e743f
Are Edvardsen <biorakel@gmail.com>
2021-10-19 13:18:59 +0200

    ifs and buts

--- tests/testthat/test-makeStandardTable.R
+++ tests/testthat/test-makeStandardTable.R
@@ -1,5 +1,6 @@
 test_that("function returns kable objects", {
   expect_true("kableExtra" %in% class(mst(tab = mtcars[1:10, ],
                                           type = "html")))
-  expect_true("knitr_kable" %in% class(mst(tab = mtcars[1:10, ])))
+  expect_true("knitr_kable" %in% class(mst(tab = mtcars[1:10, ], lsd = FALSE)))
+  expect_true("knitr_kable" %in% class(mst(tab = mtcars[1:10, ], lsd = TRUE)))
 })
