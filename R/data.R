d25b11131dbde0920fbdfbadf8ae23812f714c3b
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2025-11-10 12:36:04 +0100

    Lagt inn uttrekk av klokebok for å koble listeverdier

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Klokeboken uttrekk
+#'
+#' Et begrenset uttrekk fra klokeboken som inneholder nødvendige kolonner for
+#' å koble variabelnavn i Rapporteket med listetekst og listeverdier.
+#'
+#' @format ## `kb_smerte_uttrekk`
+#' En tibble med 1094 rader og 6 kolonner:
+#' \describe{
+#'    \item{skjemanavn}{Skjemanavn}
+#'    \item{navn_i_rapporteket}{Variabelnavn på Rapporteket}
+#'    \item{ledetekst}{ledetekst fra registerløsning}
+#'    \item{type}{Variabeltype}
+#'    \item{listeverdier}{Listeverdi}
+#'    \item{listetekst}{Tekst knyttet til listeverdi}
+#'    }
+#' @source Basert på kodebok hentet ut 2025-11-04. Ta kontakt med
+#' statistikere på fagsenteret for medisinske kvalitetsregistre, region vest
+#' om du har spørsmål eller innspill til dette datasettet.
+"kb_smerte_uttrekk"
