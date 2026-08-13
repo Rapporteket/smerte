17a68ecc1493a3d7eba22a4aea736012b582f3d7
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2026-08-13 14:42:21 +0200

    Dokumentasjon fikse_sykehusnavn og import av :=

--- R/fikse_sykehusnavn.R
+++ R/fikse_sykehusnavn.R
@@ -1,4 +1,7 @@
-#' Add variable `orgname` if missing, update values if existing
+#' Add identifying names to the UnitId
+#'
+#' @description Add variables `orgname`, `SykehusNavn` and
+#' `SykehusKortnavn` if missing, update values if existing.
 #'
 #' @param df data.frame, must contain variable `reshIdVar`
 #' @param reshIdVar Variable identifying the ReshId for the units. Can have
@@ -8,33 +11,32 @@
 #'
 #' @export
 #' @examples
-#' x <- data.frame(UnitId = as.character(c(108141, 109880, NA, 123, 105502)))
-#' x |> fikse_sykehusnavn(x, reshIdVar = "UnitId")
+#' x <- data.frame(UnitId = as.character(c(100089, 4201115, NA, 705758, 4204083)))
+#' x |> fikse_sykehusnavn(reshIdVar = "UnitId")
 #'
 fikse_sykehusnavn <- function(df, reshIdVar = UnitId) {
 
   sykehusoversikt = tribble(
     ~ "reshID", ~"orgname"             , ~"SykehusNavn"                    , ~"SykehusKortnavn",
-    0         , "Nasjonal"             , "Nasjonal"                        , "Nasjonal",
-    100089    , "Ahus"                 , "Ahus"                            , "AHUS",
-    100082    , "HUS"                  , "Haukeland"                       , "HUS",
-    4214288   , "Levanger"             , "Levanger"                        , "Levanger",
-    4201115   , "Møre og Romsdal"      , "Møre og Romsdal"                 , "Møre og Romsdal",
-    4207789   , "OUS Ullevål"          , "OUS, Ullevål"                    , "OUS, Ullevål",
-    705758    , "OUS Radiumhospitalet" , "OUS, Radiumhospitalet"           , "OUS, Radiumhospitalet",
-    705652    , "OUS Rikshospitalet"   , "OUS, Rikshospitalet"             , "OUS, Rikshospitalet",
-    100320    , "St.Olavs hospital"    , "St.Olavs hospital"               , "St.Olavs hospital",
-    101719    , "UNN"                  , "Universitetsykehuset Nord-Norge" , "UNN",
-    4204083   , "Vestre Viken"         , "Vestre Viken"                    , "Vestre Viken"   ,
-    100084    , "Fonna"                , "Fonna"                           , "Fonna"          ,
-    100133    , "Sørlandet"            , "Sørlandet"                       , "Sørlandet",
-    100083    , "Helse Stavanger"      , "Helse stavanger"                 , "SUS"
+    "0"         , "Nasjonal"             , "Nasjonal"                        , "Nasjonal",
+    "100089"    , "Ahus"                 , "Ahus"                            , "AHUS",
+    "100082"    , "HUS"                  , "Haukeland"                       , "HUS",
+    "4214288"   , "Levanger"             , "Levanger"                        , "Levanger",
+    "4201115"   , "Møre og Romsdal"      , "Møre og Romsdal"                 , "Møre og Romsdal",
+    "4207789"   , "OUS Ullevål"          , "OUS, Ullevål"                    , "OUS, Ullevål",
+    "705758"    , "OUS Radiumhospitalet" , "OUS, Radiumhospitalet"           , "OUS, Radiumhospitalet",
+    "705652"    , "OUS Rikshospitalet"   , "OUS, Rikshospitalet"             , "OUS, Rikshospitalet",
+    "100320"    , "St.Olavs hospital"    , "St.Olavs hospital"               , "St.Olavs hospital",
+    "101719"    , "UNN"                  , "Universitetsykehuset Nord-Norge" , "UNN",
+    "4204083"   , "Vestre Viken"         , "Vestre Viken"                    , "Vestre Viken"   ,
+    "100084"    , "Fonna"                , "Fonna"                           , "Fonna"          ,
+    "100133"    , "Sørlandet"            , "Sørlandet"                       , "Sørlandet",
+    "100083"    , "Helse Stavanger"      , "Helse stavanger"                 , "SUS"
   )
 
   if (!(reshIdVar %in% names(df))) stop(paste0("df must contain variable: ", reshIdVar))
 
   sykehusoversikt_temp = sykehusoversikt |>
-    mutate(reshID = as.character(reshID)) |>
     rename(!!reshIdVar := reshID)
 
   left_join(df, sykehusoversikt_temp, by = reshIdVar)
