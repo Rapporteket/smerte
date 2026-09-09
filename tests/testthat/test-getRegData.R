08a592ee753df5983cfea56c55af9ff9a559ffa6
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2026-09-09 11:21:54 +0200

    Lagt til flere tabeller i test-database og slått på igjen tester for getDataDump

--- tests/testthat/test-getRegData.R
+++ tests/testthat/test-getRegData.R
@@ -123,30 +123,30 @@ test_that("name-id mapping can be obtained", {
                "list")
 })
 
-# test_that("tables can be dumped", {
-#   check_db()
-#   # expect_equal(class(
-#   #   getDataDump("testDb", "allevar", Sys.Date(), Sys.Date())
-#   # ), "data.frame")
-#   expect_equal(class(
-#     getDataDump("testDb", tableName = "allevarnum", userRole = "SC", Sys.Date(), Sys.Date())
-#   ), "data.frame")
-#   # expect_equal(class(
-#   #   getDataDump("testDb", "avdelingsoversikt", Sys.Date(), Sys.Date())
-#   # ), "data.frame")
-#   # expect_equal(class(
-#   #   getDataDump("testDb", "forlopsoversikt", Sys.Date(), Sys.Date())
-#   # ), "data.frame")
-#   # expect_equal(class(
-#   #   getDataDump("testDb", "skjemaoversikt", Sys.Date(), Sys.Date())
-#   # ), "data.frame")
-#   expect_equal(class(
-#     getDataDump("testDb", "smertediagnoser", Sys.Date(), Sys.Date())
-#   ), "data.frame")
-#   expect_equal(class(
-#     getDataDump("testDb", "smertediagnosernum", Sys.Date(), Sys.Date())
-#   ), "data.frame")
-# })
+test_that("tables can be dumped", {
+  check_db()
+  # expect_equal(class(
+  #   getDataDump("testDb", "allevar", Sys.Date(), Sys.Date())
+  # ), "data.frame")
+  expect_equal(class(
+    getDataDump("testDb", tableName = "allevarnum", userRole = "SC", reshId = 10, Sys.Date(), Sys.Date())
+  ), "data.frame")
+  expect_equal(class(
+    getDataDump("testDb", "avdelingsoversikt", userRole = "SC", reshId = 10, Sys.Date(), Sys.Date())
+  ), "data.frame")
+  expect_equal(class(
+    getDataDump("testDb", "forlopsoversikt", userRole = "SC", reshId = 10, Sys.Date(), Sys.Date())
+  ), "data.frame")
+  # expect_equal(class(
+  #   getDataDump("testDb", "skjemaoversikt", Sys.Date(), Sys.Date())
+  # ), "data.frame")
+  expect_equal(class(
+    getDataDump("testDb", "smertediagnoser", userRole = "SC", reshId = 10, Sys.Date(), Sys.Date())
+  ), "data.frame")
+  expect_equal(class(
+    getDataDump("testDb", "smertediagnosernum", userRole = "SC", reshId = 10, Sys.Date(), Sys.Date())
+  ), "data.frame")
+})
 
 test_that("data for lokal tilsyn can be queried", {
   check_db()
