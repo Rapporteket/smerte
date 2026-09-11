6c6d9e1c40ff527746482539f9e6b787543a9833
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2026-09-11 13:18:11 +0200

    Fikse typo's og fikset argument 'd_text'.

--- R/kodebokKategoriske.R
+++ R/kodebokKategoriske.R
@@ -8,16 +8,14 @@
 #' Vær obs på at du kobler mot riktig tabell da jeg ikke har sjekket om alle
 #' kategoriske variabler har samme verdi-verditekst kombinasjon på tvers av skjema.
 #'
-#' @params d_text datasett som inneholder verditekster for listeverdiene.
+#' @param d_text datasett som inneholder verditekster for listeverdiene.
 #'
 #' @returns
 #' Returnerer en tibble med kategoriske variabler for alle tabeller oppgitt i
 #' 'tabeller'-objektet i funksjonen. Denne kan redigeres hvis nødvendig.
 #'
 #' @export
-lag_smerte_kb_kategoriske = function(
-    d_text = loadRegData(d_text = registryName, query = "SELECT * FROM text")
-    ) {
+lag_smerte_kb_kategoriske = function(d_text) {
 
   # FIXME - Denne funksjonen må oppdateres når vi får tilgang til LISTBOXTEXTROW-tabell.
   # Fungerer ikke per i dag, da text-tabell inneholder koder for rekkefølge i grensesnitt,
@@ -26,7 +24,7 @@ lag_smerte_kb_kategoriske = function(
   # Liste med tabeller vi vil ha labels for.
   tabeller = c("CENTRE", "DEPARTMENTS", "EMP11", "EMP12",
                "EMP22", "HADS", "LOCATION", "MCE",
-               "OPPIOIDOPPF", "PATEVAL", "PATIENT")
+               "OPIOIDOPPF", "PATEVAL", "PATIENT")
 
   # Labels ligger lagret i tabellen 'text' som del av en tekst-streng.
   d_text |>
