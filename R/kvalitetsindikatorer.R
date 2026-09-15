f316524738e2dc2fc384b8ba1d4a4e2b6065f144
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2026-09-15 11:53:13 +0200

    Oppdatert ki_behandlertilsyn så den passerer tester

--- R/kvalitetsindikatorer.R
+++ R/kvalitetsindikatorer.R
@@ -10,10 +10,10 @@
 #' Sosionom (`AntTilsSosio`), og Konferert med lege (`AntTilsKonfLege`).
 #'
 #' Funksjonen teller opp antall registrerte tilsyn for overnevnte variabler
-#' og returnerer `ki_nevner = TRUE/FALSE` avhengig av om pasienten skal
+#' og returnerer `ki_krit_nevner = TRUE/FALSE` avhengig av om pasienten skal
 #' inkluderes i indikator.
-#' `ki_teller = TRUE/FALSE` avhengig av om det er registrert to eller flere
-#' tilsyn og `ki_nevner = TRUE`.
+#' `ki_krit_teller = TRUE/FALSE` avhengig av om det er registrert to eller flere
+#' tilsyn og `ki_krit_nevner = TRUE`.
 #'
 #' Utdata kan videre mates inn i `rapwhale::aggreger_ki_prop()` for å beregne
 #' andel for aktuelle undergrupper.
@@ -23,12 +23,27 @@
 #' @returns
 #' Returnerer Inndatasett med følgende ekstra variabler.
 #'
-#' \item{ki_teller}{TRUE/FALSE avhengig av om pasienten skal være med i teller.}
-#' \item{ki_nevner}{TRUE/FALSE avhengig av om pasienten skal være med i nevner.}
+#' \item{ki_krit_teller}{TRUE/FALSE avhengig av om pasienten skal være med i teller.}
+#' \item{ki_krit_nevner}{TRUE/FALSE avhengig av om pasienten skal være med i nevner.}
 #' @export
 #'
 #' @examples
 #' d_behtils = ki_behandlertilsyn(d)
 ki_behandlertilsyn = function(d) {
 
+  indikatorvariabler = c("AntTilsLege", "AntTilsSykPleier", "AntTilsFysioT",
+                         "AntTilsPsyk", "AntTilsSosio", "AntTilsKonfLege")
+
+  assertthat::assert_that(all(assertthat::has_name(d, indikatorvariabler)),
+                          msg = paste0("'",
+                                       rapwhale::kjed_ord(indikatorvariabler[!indikatorvariabler %in% names(d)], "', '", "' og '"), "' må være med i inndata."))
+
+  d |> mutate(
+    across(all_of(indikatorvariabler), \(x) replace_na(x, replace = 0)),
+    ki_krit_nevner = TRUE,
+    ki_krit_teller = ki_krit_nevner &
+      (rowSums(pick(all_of(indikatorvariabler))) >= 2)
+    )
 }
+
+
--- tests/testthat/test-kvalitetsindikatorer.R
+++ tests/testthat/test-kvalitetsindikatorer.R
@@ -3,7 +3,7 @@ context("ki_behandlertilsyn")
 
 test_that("ki_behandlertilsyn gir forventet resultat", {
 
-testdata <- data.frame(
+testdata <- tibble(
     AntTilsLege      = c(0L, 1L, 2L, 5L, NA_integer_, NA_integer_),
     AntTilsSykPleier = c(0L, 0L, 0L, 0L, NA_integer_, 0L),
     AntTilsFysioT    = c(0L, 0L, 0L, 0L, NA_integer_, 0L),
@@ -12,12 +12,24 @@ testdata <- data.frame(
     AntTilsKonfLege  = c(0L, 0L, 0L, 0L, NA_integer_, 0L)
   )
 
-  expect_equal(ki_behandlertilsyn(testdata) |>
-                 select(ki_teller),
-               c(FALSE, FALSE, TRUE, TRUE, FALSE, FALSE))
-  expect_equal(ki_behandlertilsyn(testdata) |> select(ki_nevner),
-               rep(TRUE, 6))
+testresultat = tibble(
+  AntTilsLege      = c(0L, 1L, 2L, 5L, 0L, 0L),
+  AntTilsSykPleier = c(0L, 0L, 0L, 0L, 0L, 0L),
+  AntTilsFysioT    = c(0L, 0L, 0L, 0L, 0L, 0L),
+  AntTilsPsyk      = c(0L, 0L, 0L, 0L, 0L, 0L),
+  AntTilsSosio     = c(0L, 0L, 0L, 0L, 0L, 0L),
+  AntTilsKonfLege  = c(0L, 0L, 0L, 0L, 0L, 0L),
+  ki_krit_nevner   = c(rep(TRUE,6)),
+  ki_krit_teller   = c(FALSE, FALSE, TRUE, TRUE, FALSE, FALSE)
+  )
+
+  expect_identical(ki_behandlertilsyn(testdata),
+               testresultat)
+
   expect_error(ki_behandlertilsyn(testdata |> select(-AntTilsPsyk)),
-               "Variabelen 'AntTilsPsyk' må være med i inndata. ")
+               "'AntTilsPsyk' må være med i inndata.")
+  expect_error(ki_behandlertilsyn(testdata |> select(-AntTilsPsyk, -AntTilsSosio, -AntTilsKonfLege)),
+               "'AntTilsPsyk', 'AntTilsSosio' og 'AntTilsKonfLege' må være med i inndata.")
+
 
 })
