315499acbfe9fcd77b34b1b4651a6903d514e534
Endre Kvåle Evjen <endre.kvale.evjen@helse-bergen.no>
2026-09-18 22:23:32 +0200

    Oppdatert tilsynsrapport
    Lagt til tilsyn som valg i getDataDump
    Lagt inn nye variabeletiketter i tab 1 og tab 2
    Lagt til råd fra AST i tabell 3
    Oppdatert tekst for tab 3

--- R/GetRegData.R
+++ R/GetRegData.R
@@ -724,7 +724,7 @@ getDataDump <- function(registryName, reshId, userRole, tableName, fromDate, toD
                      "emp12", "emp22", "hads",
                      "mce", "opioidoppf", "pateval", "patreg")
 
-  koblet = c("allevarnum", "smertediagnosernum", "smertediagnoser", "timetodeath")
+  koblet = c("allevarnum", "smertediagnosernum", "smertediagnoser", "timetodeath", "tilsyn")
 
   if(!tableName %in% c(raadatatabeller, koblet)) {
     stop(message = "Ukjent datasett")
@@ -1317,6 +1317,35 @@ bygg_query = function(registryName, tableName, userInput) {
                    userInput)
 
   }
+
+  if(tableName == "tilsyn") {
+
+    query = paste0("SELECT
+                  emp22.CAREGIVER_DOCTOR_TIMES AS AntTilsLege,
+                  emp22.CAREGIVER_NURSE_TIMES AS AntTilsSykPleier,
+                  emp22.CAREGIVER_PHYSIOTHERAPIST_TIMES AS AntTilsFysioT,
+                  emp22.CAREGIVER_PSYCHOLOGIST_TIMES AS AntTilsPsyk,
+                  emp22.CAREGIVER_SOCIAL_WORKER_TIMES AS AntTilsSosio,
+                  emp22.CAREGIVER_DOCTOR_CONS_TIMES AS AntTilsKonfLege,
+                  emp22.CAREGIVER_DOCTOR_ADVICE_TIMES AS AntTilsRadBehandlerAST,
+                  emp22.CONSULTATIONS AS AntPasTils,
+                  mce.SUPERVISION AS Tilsett,
+                  emp11.REGISTERED_DATE AS RegDato11,
+                  mce.REGISTERED_DATE AS StartdatoTO,
+                  emp11.DEPARTMENT AS InnlAvd,
+                  mce.PATIENT_ID AS PasientID,
+                  mce.MCEID AS ForlopsID,
+                  d.ID as DEPARTMENT_ID,
+                  d.NAME AS DEPARTMENT_NAME,
+                  d.SHORTNAME AS DEPARTMENT_SHORTNAME
+                  FROM
+                  mce
+                  LEFT JOIN emp11 ON COALESCE(NULLIF(mce.PARENT_ID, 'NA'), mce.MCEID) = emp11.MCEID
+                  LEFT JOIN emp22 ON COALESCE(NULLIF(mce.PARENT_ID, 'NA'), mce.MCEID) = emp22.MCEID
+                  LEFT JOIN departments d ON emp11.DEPARTMENT = d.ID "
+                  ,
+                  userInput)
+}
 
   return(query)
 }
